acce9d2c6f7970b64177e3308b216abcac7835f1
lu10221 <shayewei6@gmail.com>
2025-12-04 00:40:58 +0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'; // 自动添加的
 
 export default defineConfig({
-  integrations: [tailwind()],
+  // 🔴 务必改成你刚刚绑定的真实域名 (带 https)
+  site: 'https://toolstock.net/', 
+  
+  integrations: [tailwind(), sitemap()],
 });
